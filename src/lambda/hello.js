36fe1a0a48a616e4ee6b41609ba7df1abe1513ef
Agboola Jude <jude.agboola@charisol.io>
2019-08-19 09:35:35 -0700

    initial commit

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,8 +1,29 @@
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+
+function launchChromeAndRunLighthouse(url, opts, config = null) {
+  return chromeLauncher
+    .launch({ chromeFlags: opts.chromeFlags, onlyCategories: ['performance'] })
+    .then(chrome => {
+      opts.port = chrome.port;
+      return lighthouse(url, opts, config).then(results => {
+        // use results.lhr for the JS-consumeable output
+        // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
+        // use results.report for the HTML/JSON/CSV output as a string
+        // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
+        return chrome.kill().then(() => results.lhr);
+      });
+    });
+}
+
 // this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
 export function handler(event, context, callback) {
-  console.log('queryStringParameters', event.queryStringParameters)
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!' }),
-  })
+  const opts = { chromeFlags: ['--show-paint-rects'] };
+  console.log('queryStringParameters', event.queryStringParameters);
+  launchChromeAndRunLighthouse('https://google.com', opts).then(result => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(result)
+    });
+  });
 }
